78ff671f39fe77bb36ef675ffca9a44735fa8b9e
MARKAPURAM VENKATA AKHIL <122592444+Akhil244@users.noreply.github.com>
2024-09-24 15:31:59 +0530

    logger and exception

---
+++ main.py
@@ -0,0 +1,19 @@
+#importing the file for using it in our process
+from sensor.exception import SensorException
+import os
+import sys
+from sensor.logger import logging
+
+def test_exception():
+    try:
+        logging.info("there is an error with dividing the no by zero")
+        a=1/0
+    except Exception as e:
+        raise SensorException(e,sys)
+
+
+if __name__=="__main__":
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
